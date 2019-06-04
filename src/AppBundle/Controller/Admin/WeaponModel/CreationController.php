9bf00a3007fb88bbf71b30b423165b6da68a07e0
Kileahh <kileahh@protonmail.ch>
2019-06-04 16:32:53 +0200

    Add WeaponModel

--- src/AppBundle/Controller/Admin/WeaponModel/CreationController.php
+++ src/AppBundle/Controller/Admin/WeaponModel/CreationController.php
@@ -11,13 +11,13 @@ class CreationController extends Controller
 {
     public function createWeaponModelAction(Request $request)
     {
-        $weapon = new WeaponModel();
-        $form = $this->createForm(CreateWeaponModelType::class, $weapon);
+        $weapon_model = new WeaponModel();
+        $form = $this->createForm(CreateWeaponModelType::class, $weapon_model);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
-            $em->persist($weapon);
+            $em->persist($weapon_model);
             $em->flush();
             return $this->redirectToRoute('app_admin_weapon_model_listing_list_weapon_model');
         }
--- src/AppBundle/Form/Type/Admin/ImplantModel/Creation/CreateImplantModelType.php
+++ src/AppBundle/Form/Type/Admin/ImplantModel/Creation/CreateImplantModelType.php
@@ -4,11 +4,9 @@
 namespace AppBundle\Form\Type\Admin\ImplantModel\Creation;
 
 
-use AppBundle\Entity\Implant;
 use AppBundle\Entity\ImplantModel;
 use AppBundle\Form\Type\Admin\File\Creation\CreateFileType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
